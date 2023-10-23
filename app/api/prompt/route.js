e30a7a60f7ef843e9746534f97e9d62001dd5bdd
GeronimoReynoso <reynoso.geronimo@gmail.com>
2023-10-22 23:22:24 -0300

    fix

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,8 +4,12 @@ import { connectToDB } from "@utils/database"
 export  const GET = async (request)=>{
     try {
         await connectToDB()
-        const prommpts = await Prompt.find({}).populate('creator')
-        return new Response(JSON.stringify(prommpts),{status:200})
+        const prompts = await Prompt.find({}).populate('creator')
+        return new Response(JSON.stringify(prompts),{status:200,headers: {
+            'Cache-Control': 'max-age=10',
+            'CDN-Cache-Control': 'max-age=60',
+            'Vercel-CDN-Cache-Control': 'max-age=3600',
+          },})
     } catch (error) {
         return new Response("Fallo al obtener los prompts ",{status:500})
     }
--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -4,8 +4,12 @@ import { connectToDB } from "@utils/database"
 export  const GET = async (request,{params})=>{
     try {
         await connectToDB()
-        const prommpts = await Prompt.find({creator:params.id}).populate('creator')
-        return new Response(JSON.stringify(prommpts),{status:200})
+        const prompt = await Prompt.find({creator:params.id}).populate('creator')
+        return new Response(JSON.stringify(prompt),{status:200,headers: {
+            'Cache-Control': 'max-age=10',
+            'CDN-Cache-Control': 'max-age=60',
+            'Vercel-CDN-Cache-Control': 'max-age=3600',
+          },})
     } catch (error) {
         return new Response("Fallo al obtener los prompts ",{status:500})
     }
